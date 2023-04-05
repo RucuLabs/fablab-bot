d1875bf63ffce5b34e4f3b6a2ecfc075c6d800b5
iPolarisu <elpolarisu@gmail.com>
2023-04-05 12:53:45 -0400

    chore: read .env file on main script

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
