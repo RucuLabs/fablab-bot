e59410c42840406da6505ca1393b3678c45e1cea
iPolarisu <elpolarisu@gmail.com>
2023-04-12 13:06:40 -0400

    chore: add commands

--- bot.py
+++ bot.py
@@ -18,7 +18,39 @@ logging.basicConfig(
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
-    start_handler = CommandHandler('start', brain.start)
-    application.add_handler(start_handler)
     
+    # command handlers
+
+    # app commands
+    start_handler = CommandHandler('start', brain.start)
+    help_handler = CommandHandler('help', brain.help_c)
+    
+    # door commands
+    open_handler = CommandHandler('open', brain.open_door)
+    door_permit_handler = CommandHandler('doorPermit', brain.add_door_permit)
+    
+    # user commands
+    add_user_handler = CommandHandler('addUser', brain.add_user)
+    edit_user_handler = CommandHandler('editUser', brain.edit_user)
+    delete_user_handler = CommandHandler('deleteUser', brain.delete_user)
+    search_user_handler = CommandHandler('searchUser', brain.search_user)
+    list_users_handler = CommandHandler('listUsers', brain.list_users)
+    
+    # adding handlers to app
+
+    # app commands
+    application.add_handler(start_handler)
+    application.add_handler(help_handler)
+    
+    # door commands
+    application.add_handler(open_handler)
+    application.add_handler(door_permit_handler)
+    
+    # user commands
+    application.add_handler(add_user_handler)
+    application.add_handler(edit_user_handler)
+    application.add_handler(delete_user_handler)
+    application.add_handler(search_user_handler)
+    application.add_handler(list_users_handler)
+
     application.run_polling()
--- brain/__init__.py
+++ brain/__init__.py
@@ -1,1 +1,13 @@
 from brain.modules.start import start
+from brain.modules.help_c import help_c
+
+from brain.modules.open_door import open_door
+from brain.modules.add_door_permit import add_door_permit
+
+from brain.modules.start import start
+from brain.modules.add_user import add_user
+from brain.modules.edit_user import edit_user
+from brain.modules.delete_user import delete_user
+from brain.modules.edit_user import edit_user
+from brain.modules.list_users import list_users
+from brain.modules.search_user import search_user
