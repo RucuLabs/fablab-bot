f6dc8f228f7a4addb3eb91ae87c99ed2db7a53c2
cristian <cristiancarrion00@gmail.com>
2023-05-16 12:46:56 -0400

    fix

--- brain/modules/add_door_permit.py
+++ brain/modules/add_door_permit.py
@@ -12,9 +12,9 @@ logger = logging.getLogger(__name__)
 
 async def add_door_permit(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
+        db = FabLabRepository()
         if((db.get_role("@" + user) == "Admin") or (db.get_role("@" + user) == "Super")):
-            user_id = context.args[0]
-            db = FabLabRepository()
+            user_id = context.args[0] 
             db.add_user_to_door(user_id)
             user = db.get_user(user_id).name
             await context.bot.send_message(chat_id=update.effective_chat.id, text="El usuario " + user + " ahora tiene permisos de puerta.")
