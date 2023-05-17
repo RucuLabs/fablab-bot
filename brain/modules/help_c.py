d94ad54c4f939dcba43d2c92cf510ac2b265f941
iPolarisu <elpolarisu@gmail.com>
2023-05-17 19:58:38 -0400

    feat: add markdown to help command

--- brain/modules/help_c.py
+++ brain/modules/help_c.py
@@ -2,19 +2,15 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 help_text = """
-    **usuarios**
-    open - abre la puerta del FabLab
-    help - información sobre los comandos
-    
-    **admin**
-    * ingrese el usuario en formato @username
-    adduser @param usuario - añade un usuario a la base de datos del FabLab
-    edituser @param usuario - edita un usuario de la base de datos del FabLab
-    deleteuser @param usuario - borra un usuario de la base de datos del FabLab
-    searchuser @param usuario - busca a un usuario en la base de datos del FabLab
-    doorpermit @param usuario - entrega permisos de acceso a un usuario del FabLab
-    listusers - muestra los ususarios registrados en la base de datos del FabLab
+*Ayuda*
+Este bot maneja la red IoT del FabLab de la FCFM. Para registrarte debes hablar con algún administrador, lo mismo para los permisos.
+
+*Comandos generales*
+help - información sobre los comandos
+
+*Comandos usuarios con acceso a puerta*
+open - abre la puerta del FabLab
 """
 
 async def help_c(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=help_text, parse_mode='Markdown')
