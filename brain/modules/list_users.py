fa04ac57d2744d3249a91f792003a90fe2adc65c
cristian <cristiancarrion00@gmail.com>
2023-06-19 21:33:51 -0400

    chore: fixed listusers permissions

--- brain/modules/list_users.py
+++ brain/modules/list_users.py
@@ -5,8 +5,12 @@ from brain.modules.data.FabLabRepository import FabLabRepository
 
 async def list_users(update: Update, context: ContextTypes.DEFAULT_TYPE):
     db = FabLabRepository()
-    users = db.get_all_users()
-    userlist = ""
-    for user in users:
-        userlist += str(user.telegram_id) + ": " + user.name + "\n"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Listando usuarios:\n" + userlist)
+    id = update.message.from_user.id
+    if((db.get_role(id) == "Admin") or (db.get_role(id) == "Super")):
+        users = db.get_all_users()
+        userlist = ""
+        for user in users:
+            userlist += str(user.telegram_id) + ": " + user.name + "\n"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Listando usuarios:\n" + userlist)
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="No tienes permisos suficientes.")
